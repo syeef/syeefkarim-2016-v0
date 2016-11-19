cba184cdf6910a369ced1954484325995e6231a2
Syeef Karim <syeefkarim@gmail.com>
2016-11-19 01:21:56 +0000

    add sticky nav

--- js/sticky.js
+++ js/sticky.js
@@ -1,6 +1,7 @@
 var  mn = $(".main-nav");
     mns = "main-nav-scrolled";
-    hdr = $('#post-background').height();
+    // hdr = $('#post-background').height();
+    hdr = 500px;
 
 $(window).scroll(function() {
   if( $(this).scrollTop() > hdr ) {
